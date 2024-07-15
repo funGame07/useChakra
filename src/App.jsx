7f7468e588063479c2889a13f5751521f46092ee
elbert <elbertchen007@gmail.com>
2024-07-15 20:43:15 +0700

    s

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./Home"
 
 function App() {
-  // <BrowserRouter>
-    //   <Routes>
-    //     {/* <Route path="/" element={<a href="/home">here</a>}/> */}
-    //     <Route path="/" element={<Home/>}/>
-    //   </Routes>
-    // </BrowserRouter>
   return(
-    <Home />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<a href="/home">here</a>}/>
+        <Route path="/" element={<Home/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
